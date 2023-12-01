a8d5bda981da615a63f5f13c0794254cfe0a2b7b
wladyDe <vovawestland@gmail.com>
2023-12-01 21:13:57 +0100

    create header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className='header'>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Volodymyr Havryliuk</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <HeaderSocials />
+        <a href="#concat" className='scroll__down'>Scroll Down</a>
+
+
+      </div>
+    </header>
   )
 }
 
