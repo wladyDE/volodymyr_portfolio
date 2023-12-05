10dca3ac7721f332990c65966d1c3610d1e1e71d
wladyDe <vovawestland@gmail.com>
2023-12-05 19:15:11 +0100

    create portfolio component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,68 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Crypto connect',
+    github: ''
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Crypto connect',
+    github: ''
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Crypto connect',
+    github: ''
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Crypto connect',
+    github: ''
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Crypto connect',
+    github: ''
+  },
+]
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className="container portfolio__container">
+        {
+          data.map(({ id, image, title, github, demo }) => {
+            return (<article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className='btn' target='_blank'>Github</a>
+                <a href={demo} target='_blank' className='btn btn-primary'>Live Demo</a>
+              </div>
+            </article>)
+          })
+        }
+      </div>
+    </section>
   )
 }
 
